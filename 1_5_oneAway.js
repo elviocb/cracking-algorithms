34d4e92eca2389182eee5d82411d83f1e496584e
Elvio Cavalcante <elviocb@Elvios-MacBook-Pro.local>
2015-08-05 01:16:16 -0300

    Add new toy problem: oneAway

---
+++ 1_5_oneAway.js
@@ -0,0 +1,17 @@
+/* There are three types of edits that can be performed on strings: 
+  - insert a character
+  - remove a character
+  - replace a character
+  Given two strings, write a function to check if they are one-edit or zero-edit away.
+
+  Example:
+
+  force  fork  --> true
+  force  forc  --> true
+  forces force --> true
+  force  fark  --> false
+*/
+
+function oneAway(){
+  
+}
